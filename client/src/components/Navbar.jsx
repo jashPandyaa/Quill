95cb240974e810d27cddb362c7cdc27ec972b257
Jash Pandya <pandyajash2005@gmail.com>
2025-07-20 15:36:53 +0530

    fixed - Navbar blogs issue

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -163,9 +163,6 @@ const Navbar = () => {
                 <Link to="/" className="text-gray-700 hover:text-blue-600 transition-colors">
                   Home
                 </Link>
-                <Link to="/user/blogs" className="text-gray-700 hover:text-blue-600 transition-colors">
-                  Blogs
-                </Link>
                 
                 {isLoggedIn ? (
                   <>
